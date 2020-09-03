6b93538a92a9ead65f8d1ee8aaf6677b9bfc70aa
Jonas <jona71e2@stud.kea.dk>
2020-09-03 03:05:43 +0200

    counting array done

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 "use strict";
 
 console.log("ArrayCount");
+
+// window.addEventListener("load", init);
+
+setInterval(init, 1000);
+
+let count = 0;
+
+const array = [];
+
+function init() {
+  count++;
+  console.log("init");
+  console.log(count);
+  array.unshift(count);
+  //array.splice(1, 0, count);
+  console.log("længde:", array.length);
+  console.log(array);
+  if (array.length > 9) {
+    array.splice(1, count);
+  }
+}
